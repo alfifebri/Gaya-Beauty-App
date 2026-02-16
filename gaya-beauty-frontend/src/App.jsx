614447df02268f2caf5cc79e6d2ee9129ef10919
alfifebri <febriawanalfi@gmail.com>
2026-02-16 21:08:17 +0700

    fix: correct import paths for customer login pages

--- gaya-beauty-frontend/src/App.jsx
+++ gaya-beauty-frontend/src/App.jsx
@@ -1,17 +1,24 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+
+// === BAGIAN INI GUE BENERIN ===
+// Kita import dari file "CustomerLogin" tapi kita namain variabelnya "LoginMember"
+import LoginMember from './pages/CustomerLogin'
+import RegisterMember from './pages/CustomerRegister'
+// ==============================
+
+import MyOrders from './pages/MyOrders'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import AdminDashboard from './pages/AdminDashboard'
-import LoginMember from './pages/LoginMember'
-import RegisterMember from './pages/RegisterMember'
-import MyOrders from './pages/MyOrders' // <--- INI WAJIB ADA!
 
 function App() {
   return (
     <Routes>
       {/* JALUR UMUM */}
       <Route path="/" element={<Home />} />
+
+      {/* Ini tetep pake LoginMember karena di atas udah kita alias-kan */}
       <Route path="/login-member" element={<LoginMember />} />
       <Route path="/register-member" element={<RegisterMember />} />
 
